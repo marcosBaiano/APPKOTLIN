dcfd40597ae421780c1be7c0af978dfa8aa984a5
rian <assisantonio495@gmail.com>
2026-06-04 16:17:08 -0300

    organização de pastas

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,3 @@
-plugins {
-    kotlin("jvm") version "2.3.20"
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     // Ktor Server
     implementation("io.ktor:ktor-server-core:2.3.7")
@@ -39,10 +28,49 @@ dependencies {
 
     // Logging
     implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("org.openfolder:kotlin-asyncapi-ktor:3.2.2")
+    implementation("io.ktor:ktor-server-cors:2.3.7")
+    implementation("io.ktor:ktor-server-caching-headers:2.3.7")
+    implementation("io.ktor:ktor-server-compression:2.3.7")
+    implementation("io.ktor:ktor-server-conditional-headers:2.3.7")
+    implementation("io.ktor:ktor-server-default-headers:2.3.7")
+    implementation("io.ktor:ktor-server-forwarded-header:2.3.7")
+    implementation("io.ktor:ktor-server-hsts:2.3.7")
+    implementation("io.ktor:ktor-server-http-redirect:2.3.7")
+    implementation("io.ktor:ktor-server-openapi:2.3.7")
+    implementation("io.ktor:ktor-server-routing-openapi:2.3.7")
+    implementation("io.ktor:ktor-server-partial-content:2.3.7")
+    implementation("com.ucasoft.ktor:ktor-simple-cache:0.72.1")
+    implementation("com.ucasoft.ktor:ktor-simple-memory-cache:0.72.1")
+    implementation("com.ucasoft.ktor:ktor-simple-redis-cache:0.72.1")
+    implementation("io.ktor:ktor-server-swagger:2.3.7")
+    implementation("io.ktor:ktor-server-auth-ldap:2.3.7")
+    implementation("io.ktor:ktor-client-core:2.3.7")
+    implementation("io.ktor:ktor-client-apache:2.3.7")
+    implementation("io.ktor:ktor-server-csrf:2.3.7")
+    implementation("io.ktor:ktor-server-sessions:2.3.7")
+    implementation("io.github.cotrin8672:ktor-line-webhook-plugin:1.5.0")
+    implementation("io.ktor:ktor-server-double-receive:2.3.7")
+    implementation("com.kborowy:firebase-auth-provider:1.6.0")
+    implementation("io.ktor:ktor-client-auth:2.3.7")
+    implementation("io.ktor:ktor-client-auth:2.3.7")
+    implementation("io.ktor:ktor-client-auth:2.3.7")
+    implementation("io.ktor:ktor-client-encoding:2.3.7")
+    implementation("io.ktor:ktor-client-logging:2.3.7")
+    implementation("io.ktor:ktor-client-resources:2.3.7")
 
     testImplementation(kotlin("test"))
 }
+plugins {
+    kotlin("jvm") version "2.3.20"
+}
 
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
 kotlin {
     jvmToolchain(21)
 }
